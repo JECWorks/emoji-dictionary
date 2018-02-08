1e675ad6ec1d8e2940ac5f0a2712f6265461649a
Jason Cox <jasoncox@mac.com>
2018-02-07 20:16:40 -0500

    Created DefinitionViewController and connected it to the 2nd ViewController and passed the defVC.emoji to it using sender

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Jason Cox on 2/7/18.
+//  Copyright © 2018 Jason Cox. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    var emoji = "NO EMOJI"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print(emoji)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
